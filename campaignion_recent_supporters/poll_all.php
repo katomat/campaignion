7e53bd4040014d3d43929052e9069da7ba9a6556
Matthias Weiss <matthias@more-onion.com>
2014-05-27 17:31:17 +0200

    [campaignion_recent_supporters] First code additions/changes for new feature   "show recent supporters for all configured action types in 1 block"

---
+++ campaignion_recent_supporters/poll_all.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file
+ *
+ * This is file does a minimal drupal bootstrap and returns the current
+ * recent supporter data.
+ */
+
+function campaignion_recent_supporters_bootstrap_inc() {
+  $dir = dirname($_SERVER['DOCUMENT_ROOT'] . $_SERVER['SCRIPT_NAME']);
+  while ($dir != '/') {
+    $bootstrap = $dir . '/includes/bootstrap.inc';
+    if (file_exists($bootstrap)) {
+      define('DRUPAL_ROOT', $dir);
+      return $bootstrap;
+    }
+    $dir = dirname($dir);
+  }
+}
+
+if ($bootstrap = campaignion_recent_supporters_bootstrap_inc()) {
+  require_once $bootstrap;
+}
+
+_drupal_bootstrap_configuration();
+_drupal_bootstrap_database();
+
+if (!isset($_GET['nid'])) {
+  exit;
+}
+
+require_once DRUPAL_ROOT . '/includes/common.inc';
+require_once dirname(__FILE__) . '/campaignion_recent_supporters.module';
+
+campaignion_recent_supporters_all_action_json($_GET['types']);
